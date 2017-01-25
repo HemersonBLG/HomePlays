29ded3eea852dab9075a8bee0a7aeba7624a9515
Hemerson Boaventura de Lima Gomes <hemerson.blg@gmail.com>
2017-01-25 12:56:29 -0200

    Update meuJS.js

--- Mercado/js/meuJS.js
+++ Mercado/js/meuJS.js
@@ -7,26 +7,34 @@ myApp.controller('Mercado', function($scope){
   $scope.produtos=""
   $scope.quantidadeProdutos=""
   $scope.pesagemProduto=""
-  var validaMarcado=
+  naoTem = false
 
   //funcionalidade do botão de incrementar um produto
   $scope.enviaProdutos = function(){
     //valida se o produto já foi inserido
-    if($scope.lista.length == 0){
-      $scope.lista.push({produto: $scope.produtos, quantidade: $scope.quantidadeProdutos, medida: $scope.pesagemProduto})
+    if(!$scope.lista.length){
+      $scope.lista.push({produto: $scope.produtos, quantidade: $scope.quantidadeProdutos, medida: $scope.pesagemProduto, IDProd: IDProduto})
       console.log(IDProduto)
       IDProduto++
-    }else {
+    }else{
       for(var i = 0; i < $scope.lista.length; i++){
         if ($scope.lista[i].produto == $scope.produtos && $scope.lista[i].medida == $scope.pesagemProduto){
         //incrementa a quantidade.
-          $scope.lista[i].quantidade += $scope.quantidadeProdutos
-        }else {
-          $scope.lista.push({produto: $scope.produtos, quantidade: $scope.quantidadeProdutos, medida: $scope.pesagemProduto})
-          console.log(IDProduto)
-          IDProduto++
-          break
+        $scope.lista[i].quantidade += $scope.quantidadeProdutos
+        console.log("aqui funfa")
+        naoTem = true
+        }else{
+          if(naoTem == true){
+            naoTem = true
+          }else{
+          naoTem = false
+          }
         }
+      }
+      if(naoTem === false){
+      $scope.lista.push({produto: $scope.produtos, quantidade: $scope.quantidadeProdutos, medida: $scope.pesagemProduto, IDProd: IDProduto})
+      }else{
+        naoTem = false
       }
     }
   }
